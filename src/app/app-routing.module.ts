45ee9b075d732d4a26c5ab68a4b86f4f9bc78ec8
Rony Cuzco <ronald.cuzco@ciberexperis.es>
2020-10-01 22:48:06 +0200

    ejemplos varios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NoticiasComponent } from './noticias/noticias.component';
 
 
-const routes: Routes = [];
+const routes: Routes =
+  [
+    { path: '', component:NoticiasComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoticiasComponent } from './noticias/noticias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoticiasComponent
   ],
   imports: [
     BrowserModule,
